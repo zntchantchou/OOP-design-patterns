d880c6633b3dc59863270e7caafb9226c88c64b6
z.ntchantchou <z.ntchantchou@groupeonepoint.com>
2020-11-22 11:04:54 +0100

    factory method pattern implemented

---
+++ factoryPattern/src/factortyPattern/GermanPizzaStore.java
@@ -0,0 +1,18 @@
+package factortyPattern;
+
+public class GermanPizzaStore extends PizzaStore {
+
+	public GermanPizzaStore() {}
+
+	public Pizza createPizza(String type) {
+		Pizza pizza = null;
+		if(type.equals("Veggie")) {
+			pizza = new VeggiePizza();
+		}
+		if(type.equals("Frida")){
+			pizza = new FridaPizza();
+		}
+		return pizza;
+	}
+	
+}
---
+++ factoryPattern/src/factortyPattern/VeggiePizza.java
@@ -0,0 +1,14 @@
+package factortyPattern;
+
+public class VeggiePizza extends Pizza {
+	public VeggiePizza() {
+		name = "Veggie";
+		dough = "Thin and lightly oiled";
+		sauce = "water";
+		toppings.add("Chili");
+		toppings.add("Peppers");
+		toppings.add("Eggs");
+		toppings.add("Mozzarella");
+	}
+}
+
